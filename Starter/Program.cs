11c16981f3d13ca45e9fe0cf7bf918216dca7bac
Yevhen Surfer <23skopets@gmail.com>
2024-03-13 11:00:59 +0100

    scores for all students

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -4,24 +4,43 @@
 int currentAssignments = 5;
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 96, 90 };
-int[] emmaScores = new int [] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int [] { 90, 95, 97, 88, 96 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
-
-decimal sophiaScore;
-
-foreach (int score in sophiaScores)
+//Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+int[] studentScores = new int [10];
+foreach (string name in studentNames)
 {
-    // add the exam score to the sum
-    sophiaSum += score;
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+    // initialize / reset the sum of scored assigments
+    int sumAssignmentScores = 0;
+    // initialize / reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
 
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 // Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
 // Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
 // Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
--- Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d435f7da45f68a63d155f9a6338b5dd2309aa95c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
