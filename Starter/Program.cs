a1f8a59f95c8ea35a7fc080b9c9fea47cb7fc17f
Yevhen Surfer <23skopets@gmail.com>
2024-03-13 11:27:31 +0100

    currentStudentLetterGrade

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -11,6 +11,11 @@ int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 //Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 int[] studentScores = new int [10];
+string currentStudentLetterGrade = "";
+
+// Display the report Header
+Console.WriteLine("Student\t\tGrade\n");
+
 foreach (string name in studentNames)
 {
     string currentStudent = name;
@@ -38,12 +43,48 @@ foreach (string name in studentNames)
     }
 
     currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
-}
 
-// Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-// Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-// Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+    
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
